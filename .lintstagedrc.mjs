534bfdd94087b8db28f191e77ac4edcdd2fbbaa8
kinvirgo <1641652086@qq.com>
2022-11-29 09:52:21 +0800

    update

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,8 @@
 export default {
-    // "fmt/**.{css,html,js,json,vue,wxml,wxss}": ["prettier --write"],
-    "fmt/**/*.js": ["prettier --write"],
-};
+	// "fmt/**.{css,html,js,json,vue,wxml,wxss}": ["prettier --write"],
+	// "fmt/**/*.js": ["prettier --write"],
+	"*.js"(files) {
+		console.log(">>> files", files)
+		return files
+	},
+}
--- fmt/code.js
+++ fmt/code.js
@@ -1,12 +1,12 @@
 function fmtJs() {
-    const str = "this is a string";
-    const num = 123;
+	const str = "this is a string"
+	const num = 123
 }
 
-const num = 1 + 100;
+const num = 1 + 100
 
 if (num) {
-    console.log(">>>");
+	console.log(">>>")
 }
 
 if ((num > 1 && 1 > 2) || true) {
@@ -14,10 +14,10 @@ if ((num > 1 && 1 > 2) || true) {
 
 // json fmt
 const json = {
-    num: "123",
-    t: 1,
-};
+	num: "123",
+	t: 1,
+}
 
-const j2 = { t: 1, m: 2 };
+const j2 = { t: 1, m: 2 }
 
-const fn = (x) => x + 1;
+const fn = x => x + 1
