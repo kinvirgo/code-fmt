1bd009708a5a33c61c69c8fa91728dbd90b4e887
kinvirgo <1641652086@qq.com>
2022-11-29 09:55:46 +0800

    update

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,7 @@
 export default {
 	// "fmt/**.{css,html,js,json,vue,wxml,wxss}": ["prettier --write"],
 	// "fmt/**/*.js": ["prettier --write"],
-	"fmt/**/*.js": ["prettier --write"],
+	"fmt/**/*.js": "prettier --write",
 	// "*.js"(files) {
 	// 	console.log(">>> files", files)
 	// 	return files
